c6d4f529f26cb8a8ffd12bafc03015dacb7fb010
ignacioborraz <ignacioborraz@hotmail.com>
2022-06-03 16:29:51 -0300

    apellidos

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import Footer from './components/Footer'
 function App() {
   let array1 = ['dolores','luciana','mayra','lau','guada','sofi']
   let array2 = ['omar','eze','elias','marco']
-  let array3 = ['apellido1', 'apellido2'];
+  let array3 = ['borraz', 'rodriguez','zaccaro'];
   return (
     <>
       <NavBar />
