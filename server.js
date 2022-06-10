8dec5067a725d1638bc47f8f9277d242edc4b695
ignacioborraz <ignacioborraz@hotmail.com>
2022-06-10 15:52:35 -0300

    dotenv

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express') //requiero el módulo de express
+const app = express() //ejecuto express para crear una app
 
-const PORT = 4000
+//creo un archivo .env y defino una variable de entorno con el puerto
+//por cuestions de seguridad el archivo .env debe ser ignorado (incluirlo en .gitignore)
+//dotenv es una librería que configura nuestra app con las variables de entorno definidas en el archivo .env
+require('dotenv').config() //requiero el módulo dotenv
+const PORT = process.env.PORT || 4000 //defino el puerto con la variable de estado "o" un numero
 
-app.set('port',PORT)
+app.set('port',PORT) //.set setea o configura una propiedad de app, en este caso el puerto
 
-app.get('/', (req, res) => {
-    res.send('EL SERVIDOR ESTÁ FUNCIONANDO!')
+app.get('/', (req, res) => { //.get lee el endpoint
+    res.send('EL SERVIDOR ESTÁ FUNCIONANDO!') // y en este caso con .send enviamos información desde el back hacia la vista
 })
 
-app.listen(app.get('port'), () => {
-    console.log('SERVIDOR CORRIENDO EN: '+app.get('port'))
+app.listen(app.get('port'), () => { //.listen escucha el puerto y lo levanta
+    console.log('SERVIDOR CORRIENDO EN: '+app.get('port')) //además me muestra por consola este texto
 })
