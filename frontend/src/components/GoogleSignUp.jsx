d48fbaf27dbbf6595c310678e95f4641948496a0
Eric Rodriguez <feric.rodriguez@gmail.com>
2022-06-28 20:56:16 -0300

    signup google

--- frontend/src/components/GoogleSignUp.jsx
+++ frontend/src/components/GoogleSignUp.jsx
@@ -1,24 +1,45 @@
-import React from 'react';
-import GoogleLogin from 'react-google-login';
-// or
-// import { GoogleLogin } from 'react-google-login';
+import React, { useEffect } from 'react';
+import jwt_decode from 'jwt-decode';
+import { useDispatch } from 'react-redux';
+import userActions from '../redux/actions/userActions'
 
-
-const responseGoogle = (response) => {
-    console.log(response);
-}
 
 export default function GoogleSignUp() {
+    const dispatch = useDispatch();
+
+
+    async function handleCallbackResponse(response) {
+        console.log(response.credential);
+        let userObject = jwt_decode(response.credential);
+        console.log(userObject);
+        dispatch(userActions.signUp({
+            nameUser: userObject.given_name,
+            lastNameUser: userObject.family_name, 
+            photoUser: userObject.picture, 
+            mail: userObject.email, 
+            password: userObject.jti, 
+            role: 'user', 
+            from: 'google'
+        }))
+    }
+
+    useEffect(() => {
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: '802279233493-ovinlm8d08qmg3o5jfo2tr3p03v2n4q5.apps.googleusercontent.com',
+            callback: handleCallbackResponse
+        });
+
+        google.accounts.id.renderButton(
+            document.getElementById('buttonDiv'),
+            { theme: "outline", size: "medium" }
+        )
+    });
 
     return (
-        <GoogleLogin
-            clientId="802279233493-ovinlm8d08qmg3o5jfo2tr3p03v2n4q5.apps.googleusercontent.com"
-            buttonText="Login"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy={'single_host_origin'}
-        />
-    );
+        <div>
+            <div id='buttonDiv'></div>
+        </div>
+    )
 }
-
 
