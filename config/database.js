244ae956f284058b6c861de21f1aec95d40892a6
ignacioborraz <ignacioborraz@hotmail.com>
2023-08-09 13:34:44 -0300

    reset

--- config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(process.env.CONECTION_MONGODB,{
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-})
-.then(()=>console.log('DATABASE CONNECTED'))
-.catch(err => console.error(err))
