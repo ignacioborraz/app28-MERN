d15f5689594ceb99219d73036e32ffe731f46079
ignacio.borraz <ignacio.borraz@koonolmexico.com>
2023-08-28 15:28:36 -0300

    crud

---
+++ controllers/itineraries/destroy.js
@@ -0,0 +1,16 @@
+import Itinerary from '../../models/Itinerary.js'
+
+export default async(req,res,next)=> {
+    try {
+        let { id } = req.params
+        let one = await Itinerary.findByIdAndDelete(id)
+        //AGREGAR CONDICIONAL POR SI NO ENCUENTRA AL ITINERARIO
+        return res.status(200).json({
+            success: true,
+            message: 'itinerary deleted',
+            response: one._id
+        })
+    } catch (error) {
+        next(error)
+    }
+}
