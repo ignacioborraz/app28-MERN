b59972a914a093899b9d095dded2e3b2de4a5ca5
ignacio.borraz <ignacioborraz@hotmail.com>
2023-09-05 11:31:12 -0300

    signin

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,39 @@
 import { Router } from "express";
-import register from "../controllers/auth/register.js";
 
+import register from "../controllers/auth/register.js";
+import signin from "../controllers/auth/signin.js";
+
+import isValidToken from "../middlewares/isValidToken.js";
+import isPassOk from "../middlewares/isPassOk.js";
+import notExistsUser from "../middlewares/notExistsUser.js";
 import isValidPass from "../middlewares/isValidPass.js";
 import existsUser from "../middlewares/existsUser.js";
 import validator from "../middlewares/validator.js";
-import registerSchema from "../schemas/register.js";
 
-let authRouter = Router()
+import registerSchema from "../schemas/register.js";
+import signinSchema from "../schemas/signin.js";
+
+let authRouter = Router();
 
 //authRouter.post('/signup',register)
 //register requiere de middlewares para VALIDAR/VERIFICAR/AUTENTICAR/AUTORIZAR/ETC
-    //validar datos con joi
-    //validar que la cuenta NO existe para que no haya RE-REGISTRO
-    //hashear la contraseña
-authRouter.post('/register',validator(registerSchema),existsUser,isValidPass,register)
+//validar datos con joi
+//validar que la cuenta NO existe para que no haya RE-REGISTRO
+//hashear la contraseña
+authRouter.post(
+  "/register",
+  validator(registerSchema),
+  existsUser,
+  isValidPass,
+  register
+);
+authRouter.post(
+  "/signin",
+  validator(signinSchema),
+  notExistsUser,
+  isPassOk,
+  isValidToken,
+  signin
+);
 
-export default authRouter
+export default authRouter;
