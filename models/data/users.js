a4c08f4de557b9db5b3cf3edba2b85e40b08ba98
IGNACIO JAVIER BORRAZ <95113020+ignacioborraz@users.noreply.github.com>
2023-09-06 15:08:15 -0300

    passport (#9)

--- models/data/users.js
+++ models/data/users.js
@@ -7,21 +7,21 @@ const users = [{
     lastName: "Borraz",
     mail: "igna@mh.com",
     photo: "https://www.cinemascomics.com/wp-content/uploads/2020/06/poder-darth-vader.jpg",
-    password: "Hola1234",
+    password: "$2b$10$y/zlddt4.qCUEMomzWNbxe2P1qPtJ7zqzb9.A/dM6u/fjrbwaIzTS",
     country: "Argentina"
 },{
     name: "Cinthia",
     lastName: "Di Risio",
     mail: "cin@mh.com",
     photo: "https://static.wikia.nocookie.net/fma/images/2/26/MouthyMei.JPG/revision/latest?cb=20111223205242&path-prefix=es",
-    password: "Hola1234",
+    password: "$2b$10$y/zlddt4.qCUEMomzWNbxe2P1qPtJ7zqzb9.A/dM6u/fjrbwaIzTS",
     country: "Argentina"
 },{
     name: "Franco",
     lastName: "Sosa",
     mail: "fran@mh.com",
     photo: "https://i.pinimg.com/originals/42/04/77/4204771e1c54c2b9afa36f668261e7bd.gif",
-    password: "Hola1234",
+    password: "$2b$10$y/zlddt4.qCUEMomzWNbxe2P1qPtJ7zqzb9.A/dM6u/fjrbwaIzTS",
     country: "Argentina"
 }]
 
