3188c7cd56a6aaf79905f0a88731cbcbad238552
ignacio.borraz <ignacioborraz@hotmail.com>
2023-09-11 16:17:43 -0300

    fix

--- controllers/likes/read.js
+++ controllers/likes/read.js
@@ -6,11 +6,11 @@ export default async (req,res,next)=> {
         if (req.query.itinerary_id) {
             queries.itinerary_id = req.query.itinerary_id
         }
-        let all = await Like.countDocuments(queries)
+        let count = await Like.countDocuments(queries)
         return res.status(200).json({
             success: true,
             message: 'likes from itinerary',
-            response: all
+            response: count
         })
     } catch (error) {
         next(error)
